7e83d1d6c5efca5d8f21d4e47dbc368b0784a010
gagandeep singh <50238950+gagandeepsingh21@users.noreply.github.com>
2020-08-31 11:05:25 +0300

    module4solution

---
+++ Module 4 submission/script.js
@@ -0,0 +1,29 @@
+var x;
+console.log(x);
+
+if(x==undefined){
+	console.log('x is undefined');
+}
+x=5;
+
+if(x==undefined){
+	console.log('x is undefined');
+}else {
+	console.log('x is defined');
+}
+/*var message ="in global";
+console.log("global message = "+ message);
+
+var a = function () {
+	var message ="inside a ";
+	console.log("global message = "+ message);
+	
+	function b () {
+	
+	console.log("inside b = "+ message);
+	
+	}
+	b();
+}
+
+a();*/
